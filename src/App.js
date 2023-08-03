f7f6cec300f4185cf8f1e700556f596f54a48ee5
akaAditya <mymail.adityapatel@gmail.com>
2023-08-03 15:51:54 +0530

    Second Commit added restaurant summary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/Layout/Navbar";
-
+import Summary from './components/Layout/Summary';
 function App() {
   return (
     <div>
       <header>
         <Navbar />
       </header>
+      <main>
+        <Summary />
+      </main>
     </div>
   );
 }
---
+++ src/components/Layout/Summary.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './Summary.css';
+const Summary = () => {
+  return (
+    <div>
+      <div className="summary-card">
+        <h2>Delicious Food, Delivered To You</h2>
+        <div className="summary-card-desc">
+          <p>
+            Choose your favourite meal from our broad selection of available
+            meals and enjoy a delicious lunch or dinner at home
+          </p>
+          <p>
+            All our meals are cooked with high-quality ingrediants, just-in-time
+            and of course by experienced chefs!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Summary;
